656ae266d9775c3e845ca9f9a78062f46717f12d
Franzkarol <franz.karols@gmail.com>
2021-04-17 20:06:56 -0300

    feature: aula 06, 05, 07

---
+++ aula05/aula05.py
@@ -0,0 +1,22 @@
+# Operadores Aritméticos
+
+"""
++, -, *, /, //(divisão inteira), **(um numero elevado a outro), %(ele retorna o modulo, ()
+"""
+
+print(10 * 10)  # Multiplicação
+print(10 + 10)  # Adição
+print(10 - 5)   # Subtração
+print(10 / 2)   # Divisão
+
+print('Luiz' + ' ' + 'Otavio')
+
+print(10 // 3)
+print(2 ** 10)
+print(10 % 3)
+print(5+2*10)
+print((5+2)*10)
+print(20*'A')
+print(20*2)
+print('20'+'A')
+print(20+2)
---
+++ aula07/aula07.py
@@ -0,0 +1,22 @@
+"""
+Iniciar com letra, pode conter números, separar _, letras minusculas
+"""
+
+nome = 'Luiz'
+idade = 32  # int
+altura = 1.80  # float
+e_maior = idade > 18  # bool
+peso = 80
+imc = 'temp'
+# data_1 = True
+# data_atual = 2019
+
+print('Nome:', nome)
+print('Idade:', idade)
+print('Altura:', altura)
+print('É maior:', e_maior)
+
+print(idade * altura)
+
+print(nome, 'tem', idade, 'de idade e seu imc é', imc)
+
