843852192123f86fe08b4f4cb11b0a1baa3a8b23
Franzkarol <franz.karols@gmail.com>
2021-04-16 23:25:55 -0300

    feature: estudando Python

---
+++ aula01/aula01.py
@@ -0,0 +1,17 @@
+# Tipos de comentários de Python
+
+"""
+Algum texto
+
+"""
+
+'''
+Um comentário
+'''
+
+print("Meu nome é Anna")
+
+# Explicando
+print(123456)
+
+print('Python é legal')  # outro comentário
