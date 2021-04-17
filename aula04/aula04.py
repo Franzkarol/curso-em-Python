397a415a73dd27e6a0fa7d2ac456927f69edc170
Franzkarol <franz.karols@gmail.com>
2021-04-17 13:09:25 -0300

    feature: aula 04

---
+++ aula04/aula04.py
@@ -0,0 +1,28 @@
+"""
+Tipos de dados primitivos
+str - string - textos 'Assim' "Assim"
+int - inteiro - 123456 0 -10 -20
+float - real / ponto flutuante - 10.50 1.5 -10.5
+bool - booleano / logico - True/False # Ele so tem dois valores Ex: 10 == 10
+"""
+
+# O pai entao e uma linguagem interpretada e uma linguagem de tipagem dinamica
+
+#print(type('Anna'))  # Dessa forma esta sendo chamada uma funcao ou uma classe. A classe seria o type
+
+# print('Luiz', type('Luiz')
+# print(10, type(10))
+# print(25.23, type(25.23))
+# print(10 == 10, type(10 == 10))
+
+# String: nome
+print('Anna', type('Anna'))
+
+# Idade: int
+print(32, type(32))
+
+# Altura: float
+print(1.80, type(1.80))
+
+# E maior de idade 10 > 20
+print(32 > 18, type(32 > 10))
