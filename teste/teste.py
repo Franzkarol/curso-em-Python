721f003e7be7437387d18a557165e3b31f18103f
Franzkarol <franz.karols@gmail.com>
2021-04-20 12:50:01 -0300

    feature: estudando Python

---
+++ teste/teste.py
@@ -0,0 +1,38 @@
+# nome = 'Otávio Miranda'
+# idade = 29
+#
+# if idade > 18:
+#     print(f'{nome} é adulto.')
+# else:
+#     print(f'{nome} NÃO é adulto.')
+#
+# if False:
+#     print('Um')
+# elif False:
+#     print('Dois')
+# elif False:
+#     print('Três')
+# elif True:
+#     print('Quatro')
+# elif False:
+#     print('Cinco')
+# else:
+#     print('Seis')
+
+# elif quando preciso verificar outras codições
+
+# Quando usar o bloco else? Quando preciso de um valor padrão ou resposta contrária à condição
+
+# num_1 = 2
+# num_2 = 1
+#
+# if num_1 = num_2:
+#     print('num_1 é igual a num_2') # -- Um erro porque a expressão está incorreta
+
+num_1 = 0
+num_2 = 0
+
+if not num_1 != num_2:
+    print('Retorno 1')
+else:
+    print('Retorno 2')
